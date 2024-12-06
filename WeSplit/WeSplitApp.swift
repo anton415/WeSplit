87a50a6b9b92e5652653e313a7be0bdc84efcf4d
Anton Serdyuchenko <anton415@gmail.com>
2024-12-06 08:22:42 +0300

    Initial Commit

---
+++ WeSplit/WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  WeSplit
+//
+//  Created by Anton Serdyuchenko on 06.12.2024.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
